d4557356e04dc79a1d8d70b1058ec73b3ee0ec83
Sharif Parish <saintparish6@gmail.com>
2025-03-03 23:52:28 -0500

    Speed Insight Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
  
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased text-stone-950 bg-stone-100`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
